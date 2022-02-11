4496a3b7b3f27c9c6485de88b44512c8f16303d6
Michael McVey <2558835m@student.gla.ac.uk>
2022-02-11 12:25:30 +0000

    Chapter 10 complete

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 
 from audioop import reverse
+from datetime import datetime
 from email.errors import CloseBoundaryNotFoundDefect
 import http
 #from nis import cat
@@ -25,11 +26,17 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list #ex6
-    return render(request, 'rango/index.html', context=context_dict)
+    
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response 
 
 
 def about(request):
-    context_dict = {'authorname' : 'Michael McVey'}
+    context_dict = {}
+    context_dict['authorname'] = 'Michael McVey'
+    context_dict['visits'] = request.session['visits']
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -147,3 +154,22 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
